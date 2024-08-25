a0efa89623e3e30207bd1b7f64d7c715fbf2ed53
Mirzya <abdullahcool0000@gmail.com>
2024-08-25 07:09:48 +0500

    Create __init__.py

---
+++ Avoids/__init__.py
@@ -0,0 +1,43 @@
+import asyncio
+import logging
+import time
+from pyrogram import Client, filters 
+from pyrogram.errors import PeerIdInvalid, ChannelInvalid, FloodWait
+from pyrogram.types import BotCommand
+from config import API_ID, API_HASH, BOT_TOKEN
+import config
+
+logging.basicConfig(
+    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s",
+    level=logging.INFO,
+    datefmt="%d-%b-%y %H:%M:%S",
+)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+LOGGER = logging.getLogger(__name__)
+
+
+app = Client(
+    "Avoids",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    in_memory=True
+)
+
+
+boot = time.time()
+async def Avoids():
+    try:
+        await app.start()
+        await asyncio.sleep(2)
+    except FloodWait as ex:
+        LOGGER.warning(ex)
+        await asyncio.sleep(ex.value)
+    print(1)
+    try:
+        LOGGER.info(f"Bot Started As {app.me.first_name}")
+    except Exception as e:
+        print(e)
+        exit()
+
+asyncio.get_event_loop().run_until_complete(Avoids())
