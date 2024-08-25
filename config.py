a218ed487266ab988980dd649e78d19dcc4fbdb8
Mirzya <abdullahcool0000@gmail.com>
2024-08-25 08:56:41 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,19 @@
 from os import getenv
+from dotenv import load_dotenv
 
-API_ID = int(getenv("API_ID", ))
+load_dotenv()
+
+API_ID = int(getenv("API_ID"))
 API_HASH = str(getenv("API_HASH", ""))
 BOT_TOKEN = str(getenv("BOT_TOKEN", ""))
-MONGO_DB_URI = str(getenv("MONGO_DB_URI", ""))
+MONGO_DB_URI = str(getenv("MONGO_DB_URI", None))
 IMAGE_URL = str(getenv("IMAGE_URL", ""))
-AWAIT_ROOM_ID = int(getenv("AWAIT_ROOM_ID", ""))
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
+AWAIT_ROOM_ID = int(getenv("AWAIT_ROOM_ID", None))
+CHANNEL_ID = int(getenv("CHANNEL_ID", None))
 
 def check():
     req = [API_ID, API_HASH, BOT_TOKEN, MONGO_DB_URI, IMAGE_URL, AWAIT_ROOM_ID, CHANNEL_ID]
-    if None in req:
-        raise ValueError("One or more required environment variables are missing. Please check your .env file.")
+    if None in req or "" in req:
+        raise ValueError("One or more required environment variables are missing or empty. Please check your .env file.")
 
 check()
